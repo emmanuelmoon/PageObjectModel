1474133e83da6ed36b77aa541c14f26eeff2d0d3
Emmanuel <emmenual123@gmail.com>
2024-11-27 21:27:45 +0500

    Added gitignore file

--- obj/Debug/net8.0/ST_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/ST_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ST_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f656aaf5d5a9fe385791902e8d356fd2803bf536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091c22f02e031da2b0db9007982e8c48a3261c29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ST_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ST_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
