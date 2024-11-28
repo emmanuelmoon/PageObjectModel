61fdccacadbb1e3265580f8214ead90a5d712287
Emmanuel <emmenual123@gmail.com>
2024-11-28 13:54:45 +0500

    Added dynamic data for login

--- TestExecution.cs
+++ TestExecution.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using ST_Project.WebApp.MensPage;
 
 namespace ST_Project;
@@ -42,30 +43,38 @@ public class UnitTest1
     #endregion
     LoginPage loginPage = new LoginPage();
     MensPage mensPage = new MensPage();
-    // [TestMethod]
-    // public void LoginWithValidEmailValidPassword()
+    public static IEnumerable<object[]> AddTestData =>
+    new List<object[]>
+    {
+        new object[] {"https://magento.softwaretestingboard.com/", "authorization-link",
+        "emmenual123@gmail.com", "qDtP6zAYSktxT1", "logged-in", "Welcome, Emmanuel Parvez!" },
+        new object [] {
+            "https://magento.softwaretestingboard.com/", "authorization-link", "emmenual1234@gmail.com",
+            "qDtP6zAYSktxT12", "messages", "The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later."
+        }
+    };
+
+
+    // public void Add_ShouldReturnCorrectSum_DynamicData(int a, int b, int expected)
     // {
-    //     loginPage.Login("https://magento.softwaretestingboard.com/", "authorization-link",
-    //     "emmenual123@gmail.com", "qDtP6zAYSktxT1");
-
-    //     string expectedText = "Welcome, Emmanuel Parvez!";
-    //     string actualText = CorePage.driver.FindElement(By.ClassName("logged-in")).Text;
-
-    //     Assert.AreEqual(expectedText, actualText);
-
-    // }
-    // [TestMethod]
-    // public void LoginWithInvalidEmailInvalidPassword()
-    // {
-    //     loginPage.Login("https://magento.softwaretestingboard.com/", "authorization-link",
-    //     "emmenual1234@gmail.com", "qDtP6zAYSktxT12");
-
-    //     string expectedText = "The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.";
-    //     string actualText = CorePage.driver.FindElement(By.ClassName("messages")).Text;
-
-    //     Assert.AreEqual(expectedText, actualText);
+    //     var calculator = new Calculator();
+    //     var result = calculator.Add(a, b);
+    //     Assert.AreEqual(expected, result);
     // }
 
+    [DataTestMethod]
+    [DynamicData(nameof(AddTestData))]
+    public void LoginWithValidEmailValidPassword(string url, string signInLink, string email, string password, string locator, string expectedText)
+    {
+        loginPage.Login(url, signInLink, email, password);
+
+        WebDriverWait wait = new WebDriverWait(CorePage.driver, new TimeSpan(0, 1, 0));
+        wait.Until(d => d.FindElement(By.ClassName(locator)));
+        string actualText = CorePage.driver.FindElement(By.ClassName(locator)).Text;
+
+        Assert.AreEqual(expectedText, actualText);
+
+    }
     [TestMethod]
     public void LoginAndAddTankFromMensPage()
     {
@@ -76,7 +85,10 @@ public class UnitTest1
         mensPage.AddArgusAllWeatherTankToCard("2");
 
         string expectedText = "You added Argus All-Weather Tank to your shopping cart.";
-        Thread.Sleep(20000);
+
+
+        WebDriverWait wait = new WebDriverWait(CorePage.driver, new TimeSpan(0, 1, 0));
+        wait.Until(d => d.FindElement(By.ClassName("messages")));
         string actualText = CorePage.driver.FindElement(By.ClassName("messages")).Text;
 
         Assert.AreEqual(expectedText, actualText);
--- obj/Debug/net8.0/ST_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/ST_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ST_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1474133e83da6ed36b77aa541c14f26eeff2d0d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd3e8912f137295ed2c74c9de3bf70280ea6149")]
 [assembly: System.Reflection.AssemblyProductAttribute("ST_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ST_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
